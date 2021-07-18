c352beaaafe654962d24826ff529ac97baa7c4b7
julessacha <fernsner.j@gmail.com>
2021-07-18 11:35:10 +0200

    package.json modification

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-
 class Project extends Component {
 
     state = {
--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,30 +1,30 @@
 export const portfolioData = [
-    {
-      id: 1,
-      name: 'Talkngo',
-      languages: ['php','symfony'],
-      languagesIcons: ['fab fa-js','fab fa-symfony3-alt'],
-      source: 'http://www.github.com',
-      info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-      picture: './media/talkngo.jpg'
-    },
-    {
-      id: 2,
-      name: 'Movizzz',
-      languages: ['javascript'],
-      languagesIcons: ['fab fa-js','fab fa-react'],
-      source: 'http://www.github.com',
-      info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
-      picture: './media/talkngo.jpg'
-    },
-    {
-      id: 3,
-      name: 'Portfolio',
-      languages: ['javascript', 'react'],
-      languagesIcons: ['fab fa-js','fab fa-react'],
-      source: 'http://www.github.com',
-      info: '',
-      picture: './media/talkngo.jpg'
-    }
-   
-  ]
+  {
+    id: 1,
+    name: 'Talkngo',
+    languages: ['php','symfony'],
+    languagesIcons: ['fab fa-js','fab fa-symfony3-alt'],
+    source: 'http://www.github.com',
+    info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium, veritatis debitis odio eveniet quos incidunt eaque nemo eius perferendis consequatur? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus laboriosam aut nihil? Possimus, id facilis dignissimos repudiandae modi dolorum ea accusantium.',
+    picture: './media/talkngo.jpg'
+  },
+  {
+    id: 2,
+    name: 'Movizzz',
+    languages: ['javascript'],
+    languagesIcons: ['fab fa-js','fab fa-react'],
+    source: 'http://www.github.com',
+    info: '',
+    picture: './media/movizzz.png'
+  },
+  {
+    id: 3,
+    name: 'Portfolio',
+    languages: ['javascript', 'react'],
+    languagesIcons: ['fab fa-js','fab fa-react'],
+    source: 'http://www.github.com',
+    info: '',
+    picture: './media/talkngo.jpg'
+  }
+ 
+]
